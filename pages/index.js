ece723137922fdeea2db88787d89cd3661e9840a
unknown <wahabahmadghori@gmail.com>
2021-05-11 20:37:03 +0500

    resource

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,25 @@
-import {Page } from "@shopify/polaris";
-import {ResourcePicker} from '@shopify/app-bridge/actions';
-
-const [isOpen, setIsOpen] = useState(false)
-const Index = () => (
-  <Page
-    primaryAction={{
-    content: 'Select Products',
-    onAction:()=>setIsOpen(true)
-  }}
-  >
-    <ResourcePicker 
-    resourceType="Product" 
-    open={isOpen} 
-    onCancel={()=>setIsOpen(false)}
-    onSelection={(payload)=>{
-      setIsOpen(false);
-      console.log(payload);
-    }}
-    />
-  </Page>
-);
+import { Page } from "@shopify/polaris";
+import {ResourcePicker} from '@shopify/app-bridge-react';
+import React ,{ useState } from 'react'
+function Index() {
+  const [isOpen, setIsOpen] = useState(false)
+  return (
+    <Page
+      primaryAction={{
+        content: 'Select Products',
+        onAction: () => setIsOpen(true)
+      }}
+    >
+      <ResourcePicker 
+      resourceType="Product" 
+      open={isOpen} 
+      onCancel={()=>isOpen(false)}
+      onSelection={(payload)=>{
+        isOpen(false)
+        console.log(payload)
+      }}/>
+    </Page>
+  )
+}
 
 export default Index;
