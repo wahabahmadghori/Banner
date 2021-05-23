d96fd834483fbb06c2097ba199b3d1f416fdb919
unknown <wahabahmadghori@gmail.com>
2021-05-23 17:53:16 +0500

    empty state fix

--- components/ProductList.js
+++ components/ProductList.js
@@ -1,36 +1,20 @@
 import React from 'react'
 import { Card, ResourceList, ResourceItem, TextStyle } from '@shopify/polaris'
+import ProductItem from '../components/ProductItem'
 
 const ProductList = ({ products }) => {
   return (
     <Card>
-      {products.length> 0?
       <ResourceList
         showHeader
         resourceName={{ singular: 'Product', plural: 'Products' }}
         items={products}
         renderItem={(product) => {
-          const { title } = product;
           return (
-            <ResourceItem
-            >
-              <h3 style={{ color: "red" }}>
-                <TextStyle variation="strong">{title}</TextStyle>
-              </h3>
-            </ResourceItem>
+            <ProductItem product={product}/>
           );
         }}
       />
-
-      :<EmptyState
-        heading="Manage your inventory transfers"
-        action={{ content: 'Add transfer' }}
-        secondaryAction={{ content: 'Learn more', url: 'https://help.shopify.com' }}
-        image="https://cdn.shopify.com/s/files/1/0262/4071/2726/files/emptystate-files.png"
-      >
-        <p>Track and receive your incoming inventory from suppliers.</p>
-      </EmptyState>
-    }
     </Card>
   )
 }
