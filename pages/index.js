f39d6cbe34b35e9df3879fc8d022d70915e259cd
unknown <wahabahmadghori@gmail.com>
2021-05-23 18:23:40 +0500

    empty state add

--- pages/index.js
+++ pages/index.js
@@ -8,26 +8,44 @@ import ProductList from "../components/ProductList";
 function Index() {
   const [isOpen, setIsOpen] = useState(false)
   const [products, setProducts] = useState([])
-  function handleProductsSelection(payload){
+  function handleProductsSelection(payload) {
     setIsOpen(false)
     setProducts(payload.selection)
     console.log(products)
   }
   return (
-    <Page
-      primaryAction={{
-        content: 'Select Products',
-        onAction: () => setIsOpen(true)
-      }}
-    >
-      <ResourcePicker
-        resourceType="Product"
-        open={isOpen}
-        onCancel={() => setIsOpen(false)}
-        onSelection={handleProductsSelection} />
+    <>
+      {products.lenght > 0 ?
+        <Page
+          primaryAction={{
+            content: 'Select Products',
+            onAction: () => setIsOpen(true)
+          }}
+        >
+          <ResourcePicker
+            resourceType="Product"
+            open={isOpen}
+            onCancel={() => setIsOpen(false)}
+            onSelection={handleProductsSelection} />
 
-        <ProductList products={products}/>
-    </Page>
+          <ProductList products={products} />
+        </Page>
+        :
+        <Card sectioned>
+          <EmptyState
+            heading="Manage your inventory transfers"
+            action={{ content: 'Add transfer' }}
+            secondaryAction={{ content: 'Learn more', url: 'https://help.shopify.com' }}
+            image="https://cdn.shopify.com/s/files/1/0262/4071/2726/files/emptystate-files.png"
+          >
+            <p>Track and receive your incoming inventory from suppliers.</p>
+          </EmptyState>
+        </Card>
+      }
+
+
+    </>
+
   )
 }
 
