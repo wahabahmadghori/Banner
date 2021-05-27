f2b452a8e065875b6dbef6a5cd458c397eaeb3af
unknown <wahabahmadghori@gmail.com>
2021-05-27 10:49:21 +0500

    clientRouter

--- pages/_app.js
+++ pages/_app.js
@@ -7,6 +7,7 @@ import { authenticatedFetch } from "@shopify/app-bridge-utils";
 import { Redirect } from "@shopify/app-bridge/actions";
 import "@shopify/polaris/dist/styles.css";
 import translations from "@shopify/polaris/locales/en.json";
+import ClientRouter from "../components/ClientRouter";
 
 function userLoggedInFetch(app) {
   const fetchFunction = authenticatedFetch(app);
@@ -61,6 +62,7 @@ class MyApp extends App {
             forceRedirect: true,
           }}
         >
+          <ClientRouter/>
           <MyProvider Component={Component} {...pageProps} shopOrigin={shopOrigin}/>
         </Provider>
       </AppProvider>
