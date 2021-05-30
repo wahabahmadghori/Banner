910136d5e03b7dd2384d978e421b3fb44bd7c20a
unknown <wahabahmadghori@gmail.com>
2021-05-30 17:14:28 +0500

    customSession

--- pages/install.js
+++ pages/install.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import {Page} from '@shopify/polaris'
 
-const Install = () => {
+const Install = ({history}) => {
+    console.log(history)
     return (
         <Page>
             Install Script
